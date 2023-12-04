02aef683b3c7dd174140ae3345f1b67ece28521e
anghelmihai2002 <Anghel.Ma.Mihai@student.utcluj.ro>
2023-12-04 12:58:22 +0200

    image carousel

--- kickoffpicks/frontend/src/Home.js
+++ kickoffpicks/frontend/src/Home.js
@@ -1,11 +1,45 @@
 import Navbar from "./components/navbar";
 import "./Home.css";
+import Box from "@mui/material/Box";
 
 export default function HomeExport() {
   return (
     <body>
       <Navbar />
-      <h1 style={{ textAlign: "center", color: "white" }}>HOME</h1>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          height: "90vh",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "red",
+            width: "65%",
+            height: "70vh",
+            borderRadius: 4,
+          }}
+        >
+          <img
+            src="test_img_3.jpg"
+            alt="Smiley face"
+            width="100%"
+            height="100%"
+            borderRadius="4"
+          ></img>
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "red",
+            width: "25%",
+            height: "70vh",
+            borderRadius: 4,
+          }}
+        ></Box>
+      </Box>
     </body>
   );
 }
---
+++ kickoffpicks/frontend/src/components/imgSlider.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import '../stiles/Slider'
+
+const Carousel = () => {
+    const data = ["1", "2", "3"]
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const carouselInfiniteScroll = () => {
+        if(currentIndex === data.length - 1){
+            return setCurrentIndex(0)
+        }
+        return setCurrentIndex(currentIndex + 1)
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() =>{carouselInfiniteScroll()}, 3000)
+        return () => clearInterval(interval)})
+    
+    return (
+        <div className='carousel-container'>
+            {data.map((item, index) => {
+                return <h1 className='carousel-item'
+                        style = {{transform: 'translate(-${currentIndex * 100}%)'}}
+                        key={index}>{item}</h1>})}
+        </div>
+    )
+}
+
+export default Carousel
